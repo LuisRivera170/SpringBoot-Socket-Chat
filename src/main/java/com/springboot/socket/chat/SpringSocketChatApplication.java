055bd1d9f28a18fa972ac8fa3048f2173d54f0cc
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2020-10-15 16:17:59 -0500

    Base project creation

---
+++ src/main/java/com/springboot/socket/chat/SpringSocketChatApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.socket.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocketChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/socket/chat/SpringSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.socket.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
